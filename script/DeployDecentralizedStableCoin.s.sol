19af88b5693c103d9f756a979002db330bd53aa6
TanishqDsharma <tanishqsharma612@gmail.com>
2024-07-06 08:07:55 +0000

    Added Deplyment Script for the DSC StableCoin

--- script/DeployDecentralizedStableCoin.s.sol
+++ script/DeployDecentralizedStableCoin.s.sol
@@ -7,12 +7,22 @@ import {Script} from "../lib/forge-std/src/Script.sol";
 
 contract DeployDecentralizedStableCoin is Script{
 
+uint256 public DEFAULT_ANVIL_KEY =0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+uint256 public deployerKey;
+
 function run() external returns(DecentralizedStableCoin){
-    
-    vm.startBroadcast();
+
+    if(block.chainid==31337){
+        deployerKey = DEFAULT_ANVIL_KEY;
+    }   else{
+        deployerKey = vm.envUint("PRIVATE_KEY");
+    }
+
+    vm.startBroadcast(deployerKey);
     DecentralizedStableCoin decentralizedStableCoin = new DecentralizedStableCoin();
     vm.stopBroadcast();
     return decentralizedStableCoin;
 }
+
 
 }
