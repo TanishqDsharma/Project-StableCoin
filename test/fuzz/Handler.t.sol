a0f3425aaebe4251159cac856a3469d37070ab88
TanishqDsharma <tanishqsharma612@gmail.com>
2024-07-15 15:48:53 +0000

    Added Invariant, OpenInvariant and handler Scripts

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,79 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.16;
+
+import {Test,console} from "../../lib/forge-std/src/Test.sol";
+import {StdInvariant} from "../../lib/forge-std/src/StdInvariant.sol";
+import { DeployDSC } from "../../script/DeployDSC.s.sol";
+import { DSCEngine } from "../../src/DSCEngine.sol";
+import {  DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import { HelperConfig } from "../../script/HelperConfig.s.sol";
+import { ERC20Mock } from "../../lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
+import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+
+contract Handler is Test {
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max; // This allows to get max uint96
+
+
+    // These are the contracts that we want handler to handle the calls too
+    constructor(
+        DSCEngine _dsce,
+        DecentralizedStableCoin _dsc
+    ) {
+        dsc=_dsc;
+        dsce=_dsce;
+        address[] memory collateralTokens =  dsce.getCollateralTokens();
+        weth=ERC20Mock(collateralTokens[0]);
+        wbtc=ERC20Mock(collateralTokens[1]);
+
+    }
+
+    // Redeem Collateral: Call this when you have collateral
+
+    function depositCollateral(
+        uint256  collateralSeed,
+        uint256 amountCollateral
+    ) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral,1,MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender,amountCollateral);
+        collateral.approve(address(dsce),amountCollateral);
+        dsce.depositCollateral(address(collateral),amountCollateral);
+        vm.stopPrank();
+    }
+
+    function redeemCollateral(
+        uint256 collateralSeed,
+        uint256 amountCollateral) public {
+            ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+            uint256 maxCollateralToRedeem =  dsce.getCollateralBalanceOfUser(address(collateral),msg.sender);
+            amountCollateral = bound(amountCollateral,0,maxCollateralToRedeem);
+            if(amountCollateral==0){
+                return;
+            }
+            dsce.redeemCollateral(address(collateral),amountCollateral);
+         }
+
+    ////////////////////////
+    /// Helper Functions////
+    ////////////////////////
+
+
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if(collateralSeed % 2 == 0 ){
+            return weth;
+        }else{
+            return wbtc;
+        }
+    }
+}
---
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -0,0 +1,62 @@
+// //SPDX-License-Identifier: MIT
+
+// // This invarinace file will have our aka our properties of system that should always hold
+
+// // Whenever writing these invariance tests always ask the question
+//     // -> What are our invariance?
+//         // -> What are the properties of the system that should always hold?
+
+// // Invariance can be
+//     //1. Total supply of DSC should be less than total value of collateral
+//     //2. Getter View Functions should never revert <-  
+
+// pragma solidity ^0.8.16;
+
+// import {Test,console} from "../../lib/forge-std/src/Test.sol";
+// import {StdInvariant} from "../../lib/forge-std/src/StdInvariant.sol";
+// import { DeployDSC } from "../../script/DeployDSC.s.sol";
+// import { DSCEngine } from "../../src/DSCEngine.sol";
+// import {  DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+// import { HelperConfig } from "../../script/HelperConfig.s.sol";
+// import { ERC20Mock } from "../../lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+// import { MockV3Aggregator } from "../mocks/MockV3Aggregator.sol";
+// import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+
+// contract OpenInvariantsTest is StdInvariant,Test {
+//    DeployDSC deployer;
+//    DecentralizedStableCoin dsc;
+//    HelperConfig config;
+//    DSCEngine dsce;
+//    address wbtc;
+//    address weth;
+
+
+//    function setUp() public {
+//         deployer = new DeployDSC();
+//         (dsc,dsce,config) = deployer.run();
+//         (,,weth,wbtc,)=config.activeNetworkConfig();
+//         targetContract(address(dsce));
+//     }
+
+//     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view{
+//         //Get value of all the collateral in the protocol
+//         // compare it to all the debt
+
+//         uint256 totalSupply = dsc.totalSupply();
+//         uint256 totalwethdeposited = IERC20(weth).balanceOf(address(dsce));
+//         uint256 totalwbtcdeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+//         uint256 wethvalue = dsce.getUsdValue(weth,totalwethdeposited);
+//         uint256 wbtcvalue = dsce.getUsdValue(wbtc,totalwbtcdeposited);
+
+//         console.log("WETH value is: ",weth);
+//         console.log("WBTC value is: ",wbtc);
+//         console.log("Total Supply is :", totalSupply);
+
+//         assert(wethvalue+wbtcvalue>=totalSupply);
+
+
+//     }
+
+// }
