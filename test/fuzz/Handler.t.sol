cec1569b132ddd48762c974887ace344f8928f04
TanishqDsharma <tanishqsharma612@gmail.com>
2024-09-07 18:18:06 +0000

    Handler test file for invariant testing

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+//Handler is going to handle the way we call functions
+
+import {Test,console} from "../../lib/forge-std/src/Test.sol";
+import {StdInvariant} from "../../lib/forge-std/src/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../../lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+ 
+// We want to make sure that redeemCollateral is not being, before depositing the collateral. We only want 
+// to call redeemCollateral if there is collateral init. So, this contract is gonna do that for us
+
+// We need make the constructor so that the handler contract knows what the dsce engine even is
+
+DSCEngine dsce;
+DecentralizedStableCoin dsc;
+
+ERC20Mock weth;
+ERC20Mock wbtc;
+
+uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dsce, DecentralizedStableCoin _dsc){
+    // These are the contract that we want handler to handle making the calls to
+            dsce=_dsce;
+            dsc=_dsc;
+
+            address[] memory collateralTokens = dsce.getCollateralTokens();
+            weth = ERC20Mock(collateralTokens[0]);
+            wbtc = ERC20Mock(collateralTokens[1]);
+
+
+    }
+
+    // So in your handlers whatever parameters you have are going to be randomized. So, we will make a random valid collateral to
+    // deposit and then we will pick a random valid amountCollateral to deposit
+    function depositCollateral(uint256 collateralSeed,uint256 amountCollateral) public{
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        // We are bounding are amountCollateral to a specific range so that it only have valid random values
+        amountCollateral = _bound(amountCollateral,1,MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender,amountCollateral);
+        collateral.approve(address(dsce), amountCollateral);
+        dsce.depositCollateral(address(collateral),amountCollateral);
+    }
+
+
+
+    ////////////////////////
+    ///Helper Functions////
+    ///////////////////////
+
+    //This function will ensure that we will get valid collateral type not just any random invalid collateral types.
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns(ERC20Mock){
+        if(collateralSeed%2==0){
+            return weth;
+        }
+    return wbtc;
+     
+    }
+}
---
+++ test/fuzz/Invariants.t.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+
+
+//  What are invariants?
+// - The total supply of dsc should be less than the total value of collateral
+// - Are getter functions should never revert
+
+
+import {Test,console} from "../../lib/forge-std/src/Test.sol";
+import {StdInvariant} from "../../lib/forge-std/src/StdInvariant.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {Handler} from "../fuzz/Handler.t.sol";
+
+//NOTE: This StdInvariant provides a targetContract function: which says this the target contract 
+// I want you to call all these random functions on 
+
+contract InvariantsTest is StdInvariant,Test {
+
+DeployDSC deployer;
+DSCEngine dsce;
+DecentralizedStableCoin dsc;
+HelperConfig config;
+address weth;
+address wbtc;
+Handler handler;
+    function setUp() external{
+        deployer = new DeployDSC();
+
+        (dsc,dsce,config) = deployer.run();
+
+        (,,weth,wbtc,)=config.activeNetworkConfig();
+        
+        //targetContract(address(dsce));  -> Now, in handler testing we do something different
+        handler = new Handler(dsce,dsc);
+        //Now, we will say our target contract is handler
+        targetContract(address(handler));
+        
+    }
+
+     function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // Get the value of all of the collateral in the protocol
+        // compare it to all the debt
+        uint256 totalSupply = dsc.totalSupply();
+        uint256 totalWethDeposited = IERC20(weth).balanceOf(address(dsce));
+        uint256 totalWbtcDeposited = IERC20(wbtc).balanceOf(address(dsce));
+
+        uint256  wethvalue = dsce.getUsdValue(weth,totalWethDeposited);
+        uint256 wbtcvalue = dsce.getUsdValue(wbtc,totalWbtcDeposited);
+
+        console.log("weth value is:",wethvalue);
+        console.log("wbtc value is:",wbtcvalue);
+        console.log("TotalSupply value is:",totalSupply);
+
+        assert(wethvalue+wbtcvalue>=totalSupply);
+
+
+
+    }
+
+}
